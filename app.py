2b4abf0813d87be1ada4e6f2d975172805d3e2ae
eslamfouad20384-hub <eslam.fouad20384@gmail.com>
2026-09-09 22:50:22 +0300

    Add 'INEG.CA' to EGX100 list

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ st.caption(
 
 EGX100 = [
     "COMI.CA", "MFPC.CA", "PHDC.CA", "ORAS.CA", "HDBK.CA", 
-    "EFIH.CA", "AMES.CA",
+    "EFIH.CA", "AMES.CA", "INEG.CA",
     "BTFH.CA", "BIOC.CA",
     "CLHO.CA", "MBSC.CA",  
     "MTIE.CA", "EGTS.CA", "EGSA.CA", 
